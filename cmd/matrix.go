35126376f5aac992942c225cff361bf10a00229a
grassblock <i@gb0.dev>
2026-03-02 14:28:53 +0800

    feat: add tool for getting info of a matrix homeserver

---
+++ cmd/matrix.go
@@ -0,0 +1,75 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// matrixCmd represents the matrix command
+var matrixCmd = &cobra.Command{
+	Use:   "matrix",
+	Short: "Get information of a matrix homeserver",
+	Long:  `Get a server domain behind a matrix homeserver and the software powers it`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		domain := args[0]
+		homeserver, software, version := getServerInfo(domain)
+		fmt.Printf("%s is behind %s, running %s (%s).", domain, homeserver, software, version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(matrixCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// matrixCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// matrixCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func getServerInfo(domain string) (string, string, string) {
+	wellKnownURL := fmt.Sprintf("https://%s/.well-known/matrix/client", domain)
+	resp, err := http.Get(wellKnownURL)
+	if err != nil {
+		log.Fatal("Error requesting well-known URL: ", err)
+	}
+	defer resp.Body.Close()
+	var data interface{}
+	e := json.NewDecoder(resp.Body).Decode(&data)
+	if e != nil {
+		log.Fatal("Error while parsing well-known JSON: ", e)
+	}
+	dMap := data.(map[string]interface{})
+	homeserverData := dMap["m.homeserver"].(map[string]interface{})
+	homeserver := homeserverData["base_url"].(string)
+	// get from federation api
+	federationURL := fmt.Sprintf("%s/_matrix/federation/v1/version", homeserver)
+	fresp, ferr := http.Get(federationURL)
+	if ferr != nil {
+		log.Fatal(err)
+	}
+	defer fresp.Body.Close()
+	var softwareData interface{}
+	fe := json.NewDecoder(fresp.Body).Decode(&softwareData)
+	if fe != nil {
+		log.Fatal(fe)
+	}
+	softwareDataMap := softwareData.(map[string]interface{})
+	// dealing with nesting data
+	serverMap := softwareDataMap["server"].(map[string]interface{})
+	software := serverMap["name"].(string)
+	version := serverMap["version"].(string)
+	return homeserver, software, version
+}

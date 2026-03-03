66f97b6fe94e3baedb3f46aef2e30ca1553d557c
grassblock <i@gb0.dev>
2026-03-03 21:52:53 +0800

    feat: add version subcommand

--- cmd/root.go
+++ cmd/root.go
@@ -8,9 +8,10 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "gb",
-	Short: "gb is a cli tool for everyday tasks",
-	Long:  "gb is a cli tool for everyday tasks - base32/64/85 encoding/decoding, getting quotes, etc",
+	Use:     "gb",
+	Short:   "gb is a cli tool for everyday tasks",
+	Long:    "gb is a cli tool for everyday tasks - base32/64/85 encoding/decoding, getting quotes, etc",
+	Version: Version,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := cmd.Help()
 		if err != nil {
---
+++ cmd/version.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"runtime"
+	"runtime/debug"
+
+	"github.com/spf13/cobra"
+)
+
+var commit = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return "g" + setting.Value[0:8]
+			}
+		}
+	}
+
+	return "devel"
+}()
+
+var Version = "0.0.0-" + commit
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version and more info",
+	Long:  `Prints the version and more info`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("gb version %s (on %s %s)\n", Version, runtime.GOOS, runtime.GOARCH)
+		reader := rand.Reader
+		n, _ := rand.Int(reader, big.NewInt(1000))
+		if n.Int64() == 0 {
+			fmt.Printf("\nCake is a lie.\n")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
